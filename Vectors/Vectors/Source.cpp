c5f0e0c0ee0edb836fca2cb219ac8ca197c8c1d4
regireed89 <regireed89@gmail.com>
2016-10-28 12:40:31 -0500

    vectors

--- Vectors/Vectors/Source.cpp
+++ Vectors/Vectors/Source.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <Math.h>
+#include <cassert>
 using namespace std;
 
 class Vector2
 {
 
 private:
-	
+
 	float x, y;
 
 public:
-	Vector2(float a, float b) 
+	Vector2(float a, float b)
 	{
 		x = a;
 		y = b;
@@ -18,14 +19,14 @@ public:
 
 	Vector2 add(Vector2 other)
 	{
-		
+
 		float a = this->x + other.x;
 		float b = this->y + other.y;
-		
+
 		return Vector2(a, b);
 	}
 
-	Vector2 subtract(Vector2 other) 
+	Vector2 subtract(Vector2 other)
 	{
 		float a = x - other.x;
 		float b = y - other.y;
@@ -41,36 +42,44 @@ public:
 		return Vector2(a, b);
 	}
 
-	Vector2 Magnitude(Vector2 A)
+	float Magnitude()
 	{
-		float a = sqrt(x * A.x);
-		float b = sqrt(x * A.x);
+		 float a = sqrt(x*x + y*y);
+		
 
-		return Vector2(a, b);
+		 return a;
 	}
 
-	Vector2 Normal(Vector2 A)
+	float Normal()
 	{
-
+		float a = x / Magnitude();
+		float b = y / Magnitude();
+		
+		return a, b;
 	}
 
-	Vector2 DotProduct(Vector2 A)
+	Vector2 DotProduct(Vector2 other)
 	{
+		float a = 
+			float b;
 
+		return other;
 	}
 
 };
 
 int main()
 {
-	Vector2 A = Vector2(4.5, 8.2);
+	Vector2 A = Vector2(4.5, 5.2);
 	Vector2 B = Vector2(6.3, 2.4);
 	Vector2 C = A.add(B);
 	Vector2 D = A.subtract(B);
 	Vector2 E = A.ScalarMult(B);
-	Vector2 F = A.Magnitude(B);
-	Vector2 G = A.Normal(B);
+	float F = A.Magnitude();
+	float G = A.Normal();
 	Vector2 H = A.DotProduct(B);
+	
+	assert(1 + 1 == 2);
 
 
 	system("pause");
