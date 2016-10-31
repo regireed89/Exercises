454b1efbb0651dbbefc337048302864d763d629f
regireed89 <regireed89@gmail.com>
2016-10-31 13:30:57 -0500

    linked

---
+++ Linked/Linked/Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+template<typename Type>
+class Node
+{
+protected:
+	int count;
+	nodeType<Type>*first;
+	nodeType<Type>*last;
+public:
+
+	bool Add(const&Type);
+	LinkedList;
+	const Type front();
+	const int length();
+	void Print();
+
+
+
+};
+
+
+int main()
+{
+
+}
