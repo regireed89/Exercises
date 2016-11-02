80f1b4aef68eefc5fc8e77045777fb33c6dad1e0
regireed89 <regireed89@gmail.com>
2016-11-02 14:56:10 -0500

    linked

---
+++ Linked/morelinked/morelinked/Header.h
@@ -0,0 +1,43 @@
+#pragma once
+#include<iostream>
+
+template<typename Type>
+struct nodeType
+{
+	Type info;
+	nodeType<Type> *link;
+};
+
+template<typename Type>
+class linkedListIterator
+{
+private:
+	nodeType<Type> * current;
+public:
+
+	linkedListIterator()
+	{
+		current = NULL;
+	}
+	linkedListIterator(nodeType<Type> *node)
+	{
+		current = node;
+	}
+	Type operator*()
+	{
+
+
+		
+	}
+
+
+
+
+
+
+
+
+
+
+};
+
