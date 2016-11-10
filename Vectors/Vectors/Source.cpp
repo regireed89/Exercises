92b11835f3cc88bc782c338aaec308092b845956
regireed89 <regireed89@gmail.com>
2016-11-10 17:45:18 -0600

    vector

--- Vectors/Vectors/Source.cpp
+++ Vectors/Vectors/Source.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <Math.h>
 #include <cassert>
+#include<stdio.h>
+
+
 using namespace std;
 
 class Vector2
@@ -51,12 +54,11 @@ public:
 		return a;
 	}
 
-	float Normalize()
+	Vector2 Normalize()
 	{
-		float a = x / Magnitude();
-		float b = y / Magnitude();
+		Vector2 vec = Vector2(x / Magnitude(), y / Magnitude());
 
-		return a, b;
+		return vec;
 	}
 
 	float DotProduct(Vector2 &other)
@@ -120,13 +122,11 @@ public:
 		return a;
 	}
 
-	float Normalize()
+	Vector3 Normalize()
 	{
-		float a = x / Magnitude();
-		float b = y / Magnitude();
-		float c = z / Magnitude();
+		Vector3 vec = Vector3(x / Magnitude(), y / Magnitude(), z / Magnitude());
 
-		return a, b, c;
+		return vec;
 	}
 
 	float DotProduct(Vector3 &other)
@@ -140,8 +140,18 @@ public:
 		return product;
 	}
 
-};
+	Vector3 crossProduct(Vector3 &other)
+	{
+		Vector3 vec = Vector3(y * other.z - z * other.y, x * other.z - z * other.z, x * other.y - y * other.x);
 
+		return vec;
+	}
+
+
+
+
+
+};
 class Vector4
 {
 private:
@@ -191,14 +201,11 @@ public:
 		return a;
 	}
 
-	float Normalize()
+	Vector4 Normalize()
 	{
-		float a = x / Magnitude();
-		float b = y / Magnitude();
-		float c = z / Magnitude();
-		float d = w / Magnitude();
+		Vector4 vec = Vector4(x / Magnitude(), y / Magnitude(), z / Magnitude(), w / Magnitude());
 
-		return a, b, c, d;
+		return vec;
 	}
 
 	float DotProduct(Vector4 &other)
@@ -214,29 +221,50 @@ public:
 	}
 
 
-};
 
+};
 class Matrix2D
 {
+
+private:
+	int * m_data;
 public:
 	Matrix2D() {};
-	Matrix2D(float a, float b, float c, float d) 
-	{	
-
-		float Mat2D[4] = { a, b, c, d };
-
+	Matrix2D(int a, int b, int c, int d)
+	{
+		m_data = new int[4];
+		m_data[0] = a;
+		m_data[1] = b;
+		m_data[2] = c;
+		m_data[3] = d;
 	};
 
 
+	Matrix2D Multiply(Matrix2D &other)
+	{
+		Matrix2D Mat = Matrix2D(m_data[0] * other.m_data[0] + m_data[1] * other.m_data[2],
+			m_data[0] * other.m_data[1] + m_data[1] * other.m_data[3],
+			m_data[2] * other.m_data[0] + m_data[3] * other.m_data[2],
+			m_data[2] * other.m_data[1] + m_data[3] * other.m_data[3]);
+
+		return Mat;
+	}
+	
+	void PirntStuff()
+	{
+		for (int i = m_data[0]; i < m_data[4]; i++)
+		{
+			cout << m_data[i];
+			if (m_data[i] % 2)
+			{
+				cout << endl;
+			}
+		}
+	}
 
 
 
 };
-
-
-
-
-
 int main()
 {
 	Vector2 Vec2A = Vector2(4, 5);
@@ -246,8 +274,8 @@ int main()
 	Vector2 Vec2Mult = Vec2A.ScalarMult(Vec2B);
 	float Vec2AMag = Vec2A.Magnitude();
 	float Vec2BMag = Vec2B.Magnitude();
-	float Vec2ANorm = Vec2A.Normalize();
-	float Vec2BNorm = Vec2B.Normalize();
+	Vector2 Vec2ANorm = Vec2A.Normalize();
+	Vector2 Vec2BNorm = Vec2B.Normalize();
 	float Vec2Dot = Vec2A.DotProduct(Vec2B);
 
 
@@ -258,11 +286,28 @@ int main()
 	Vector3 Vec3Mult = Vec3A.ScalarMult(Vec3B);
 	float Vec3KMag = Vec3A.Magnitude();
 	float Vec3LMag = Vec3B.Magnitude();
-	float Vec3KNorm = Vec3A.Normalize();
-	float Vec3LNorm = Vec3B.Normalize();
-	float Vec3Dot = Vec3A.DotProduct(Vec3B);
+	Vector3 Vec3ANorm = Vec3A.Normalize();
+	Vector3 Vec3BNorm = Vec3B.Normalize();
+	float Vec3Dot = Vec3ANorm.DotProduct(Vec3BNorm);
+	Vector3 Vec3Cross = Vec3A.crossProduct(Vec3B);
 
 
+	Vector4 Vec4A = Vector4(4, 5, 6, 7);
+	Vector4 Vec4B = Vector4(6, 2, 9, 3);
+	Vector4 Vec4Add = Vec4A.add(Vec4B);
+	Vector4 Vec4Sub = Vec4A.subtract(Vec4B);
+	Vector4 Vec4Mult = Vec4A.ScalarMult(Vec4B);
+	float Vec4KMag = Vec4A.Magnitude();
+	float Vec4LMag = Vec4B.Magnitude();
+	Vector4 Vec4ANorm = Vec4A.Normalize();
+	Vector4 Vec4BNorm = Vec4B.Normalize();
+	float Vec4Dot = Vec4ANorm.DotProduct(Vec4BNorm);
+
+
+	Matrix2D MatA = Matrix2D(1, 0, 1, 0);
+	Matrix2D MatB = Matrix2D(1, 2, 3, 4);
+	Matrix2D MatMult = MatA.Multiply(MatB);
+	MatA.PirntStuff();
 
 
 
