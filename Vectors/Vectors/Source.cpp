beb64c7a2e6546f49fc012cbd70a94b38fe6230d
regireed89 <regireed89@gmail.com>
2016-11-17 11:38:27 -0600

    vector

--- Vectors/Vectors/Source.cpp
+++ Vectors/Vectors/Source.cpp
@@ -64,13 +64,20 @@ public:
 		return c;
 	}
 
+	void Print()
+	{
+		cout << "2D Vector" << endl;
+		cout << "x=" << x << "y=" << y << endl;
+		cout << "____" << endl;
+	}
+
 };
 class Vector3
 {
 
 private:
 
-	
+
 
 public:
 	float x, y, z;
@@ -142,7 +149,12 @@ public:
 		return vec;
 	}
 
-
+	void Print()
+	{
+		cout << "3D Vector" << endl;
+		cout << "x=" << x << "y=" << y << "z=" << z << endl;
+		cout << "____" << endl;
+	}
 
 
 
@@ -150,7 +162,7 @@ public:
 class Vector4
 {
 private:
-	
+
 public:
 	float x, y, z, w;
 	Vector4(float a, float b, float c, float d)
@@ -215,7 +227,12 @@ public:
 
 		return product;
 	}
-
+	void Print()
+	{
+		cout << "4D Vector" << endl;
+		cout << "x=" << x << "y=" << y << "z=" << z << "w=" << w << endl;
+		cout << "____" << endl;
+	}
 
 
 };
@@ -253,7 +270,7 @@ public:
 
 
 
-	void PirntStuff()
+	void PirntMatrix2()
 	{
 		cout << "2D Matrix" << endl;
 		cout << m_data[0] << " " << m_data[1] << "\n" << m_data[2] << " " << m_data[3] << endl;
@@ -299,27 +316,20 @@ public:
 	{
 		float a = m_data[0] * vec.x + m_data[1] * vec.y + m_data[2] * vec.z;
 		float b = m_data[3] * vec.x + m_data[4] * vec.y + m_data[5] * vec.z;
-		float c = m_data[]
+		float c = m_data[6] * vec.x + m_data[7] * vec.y + m_data[8] * vec.z;
 
-
+		return Vector3(a, b, c);
 
 	}
+	void PrintMatrix3()
+	{
 
 
+		cout << "3D Matrix" << endl;
+		cout << m_data[0] << " " << m_data[1] << " " << m_data[2] << "\n" << m_data[3] << " " << m_data[4] << " " << m_data[5] << "\n" << m_data[6] << " " << m_data[7] << " " << m_data[8] << endl;
+		cout << "______" << endl;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+	}
 
 
 };
@@ -337,6 +347,27 @@ int main()
 	Vector2 Vec2ANorm = Vec2A.Normalize();
 	Vector2 Vec2BNorm = Vec2B.Normalize();
 	float Vec2Dot = Vec2ANorm.DotProduct(Vec2BNorm);
+	cout << "2D Vectors" << endl;
+	cout << "first 2d vector" << endl;
+	Vec2A.Print();
+	cout << "second 2d vector" << endl;
+	Vec2B.Print();
+	cout << "both 2d vectors added together" << endl;
+	Vec2Add.Print();
+	cout << "second 2d vector subtracted from first 2d vector" << endl;
+	Vec2Sub.Print();
+	cout << "both 2d vectors multiplied" << endl;
+	Vec2Mult.Print();
+	cout << "magnitude of first 2d vector" << endl;
+	cout << Vec2AMag << endl;
+	cout << "magnitude of second 2d vector" << endl;
+	cout << Vec2BMag << endl;
+	cout << "first 2d vector normalized" << endl;
+	Vec2ANorm.Print();
+	cout << "second 2d vector normalized" << endl;
+	Vec2BNorm.Print();
+	cout << "the dot product of both vectors" << endl;
+	cout << Vec2Dot << endl;
 
 
 	Vector3 Vec3A = Vector3(4, 5, 6);
@@ -344,24 +375,70 @@ int main()
 	Vector3 Vec3Add = Vec3A.add(Vec3B);
 	Vector3 Vec3Sub = Vec3A.subtract(Vec3B);
 	Vector3 Vec3Mult = Vec3A.ScalarMult(Vec3B);
-	float Vec3KMag = Vec3A.Magnitude();
-	float Vec3LMag = Vec3B.Magnitude();
+	float Vec3AMag = Vec3A.Magnitude();
+	float Vec3BMag = Vec3B.Magnitude();
 	Vector3 Vec3ANorm = Vec3A.Normalize();
 	Vector3 Vec3BNorm = Vec3B.Normalize();
 	float Vec3Dot = Vec3ANorm.DotProduct(Vec3BNorm);
 	Vector3 Vec3Cross = Vec3A.crossProduct(Vec3B);
-
+	cout << "||||||||||||||||||||||||||||||||||||||||" << endl;
+	cout << "3D Vectors" << endl;
+	cout << "first 3d vector" << endl;
+	Vec3A.Print();
+	cout << "second 3d vector" << endl;
+	Vec3B.Print();
+	cout << "both 3d vectors added together" << endl;
+	Vec3Add.Print();
+	cout << "second 3d vector subtracted from first 2d vector" << endl;
+	Vec3Sub.Print();
+	cout << "both 3d vectors multiplied" << endl;
+	Vec3Mult.Print();
+	cout << "magnitude of first 3d vector" << endl;
+	cout << Vec3AMag << endl;
+	cout << "magnitude of second 3d vector" << endl;
+	cout << Vec3BMag << endl;
+	cout << "first 3d vector normalized" << endl;
+	Vec3ANorm.Print();
+	cout << "second 3d vector normalized" << endl;
+	Vec3BNorm.Print();
+	cout << "the dot product of both vectors" << endl;
+	cout << Vec3Dot << endl;
+	cout << "the cross product of both vectors" << endl;
+	Vec3Cross.Print();
 
 	Vector4 Vec4A = Vector4(4, 5, 6, 7);
 	Vector4 Vec4B = Vector4(6, 2, 9, 3);
 	Vector4 Vec4Add = Vec4A.add(Vec4B);
 	Vector4 Vec4Sub = Vec4A.subtract(Vec4B);
 	Vector4 Vec4Mult = Vec4A.ScalarMult(Vec4B);
-	float Vec4KMag = Vec4A.Magnitude();
-	float Vec4LMag = Vec4B.Magnitude();
+	float Vec4AMag = Vec4A.Magnitude();
+	float Vec4BMag = Vec4B.Magnitude();
 	Vector4 Vec4ANorm = Vec4A.Normalize();
 	Vector4 Vec4BNorm = Vec4B.Normalize();
 	float Vec4Dot = Vec4ANorm.DotProduct(Vec4BNorm);
+	cout << "||||||||||||||||||||||||||||||||||||||||" << endl;
+	cout << "4D Vectors" << endl;
+	cout << "first 4d vector" << endl;
+	Vec4A.Print();
+	cout << "second 4d vector" << endl;
+	Vec4B.Print();
+	cout << "both 4d vectors added together" << endl;
+	Vec4Add.Print();
+	cout << "second 4d vector subtracted from first 2d vector" << endl;
+	Vec4Sub.Print();
+	cout << "both 4d vectors multiplied" << endl;
+	Vec4Mult.Print();
+	cout << "magnitude of first 4d vector" << endl;
+	cout << Vec4AMag << endl;
+	cout << "magnitude of second 4d vector" << endl;
+	cout << Vec4BMag << endl;
+	cout << "first 4d vector normalized" << endl;
+	Vec4ANorm.Print();
+	cout << "second 4d vector normalized" << endl;
+	Vec4BNorm.Print();
+	cout << "the dot product of both vectors" << endl;
+	cout << Vec4Dot << endl;
+	
 
 
 	Matrix2 MatA2 = Matrix2(4, 3, 2, 1);
@@ -369,22 +446,25 @@ int main()
 	Vector2 Vec2 = Vector2(8, 5);
 	Matrix2 Mat2Mult = MatA2.Multiply(MatB2);
 	Vector2 VecxMat = MatA2.Vec2xMat2(Vec2);
-	cout << "first matrix" << endl;
-	MatA2.PirntStuff();
-	cout << "second matrix" << endl;
-	MatB2.PirntStuff();
-	cout << "multiplying the two above matricies together" << endl;
-	Mat2Mult.PirntStuff();
+	cout << "first 2d matrix" << endl;
+	MatA2.PirntMatrix2();
+	cout << "second 2d matrix" << endl;
+	MatB2.PirntMatrix2();
+	cout << "multiplying the two above 2d matricies together" << endl;
+	Mat2Mult.PirntMatrix2();
 
 
-	Matrix3 MatA3 = Matrix3(1, 2, 3, 4, 5, 6, 7, 8, 9);
+	Matrix3 MatA3 = Matrix3(7, 8, 4, 8, 9, 4, 7, 6, 2);
 	Matrix3 MatB3 = Matrix3(9, 8, 7, 6, 5, 4, 3, 2, 1);
 	Vector3 Vec3 = Vector3(3, 7, 0);
 	Matrix3 Mat3Mult = MatA3.Multiply(MatB3);
-
-
-	
-
+	Vector3 Vec3xMat = MatA3.Vec3xMat3(Vec3);
+	cout << "first 3d matrix" << endl;
+	MatA3.PrintMatrix3();
+	cout << "second 3d matrix" << endl;
+	MatB3.PrintMatrix3();
+	cout << "multiplying the two above 3d matricies together" << endl;
+	Mat3Mult.PrintMatrix3();
 
 	system("pause");
 	return 0;
