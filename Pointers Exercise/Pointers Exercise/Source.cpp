ab99f864f3e0914a2c58448006cdf3ed96829124
regireed89 <regireed89@gmail.com>
2016-12-10 18:56:21 -0600

    pointer

--- Pointers Exercise/Pointers Exercise/Source.cpp
+++ Pointers Exercise/Pointers Exercise/Source.cpp
@@ -17,17 +17,17 @@ int main()
 
 	p3 = &d;
 
-	cout << "*p3 = " << *p3 << endl; // *p3 = S
+	cout << "*p3 = " << *p3 << endl; // *p3 = 9772
 
 	p3 = p1;
 
-	cout << "*p3 = " << *p3; // *p3 = T
+	cout << "*p3 = " << *p3; // *p3 = 6940
 
 	cout << ", p3 = " << p3 << endl; // , p3 = T
 
 	*p1 = *p2;
 
-	cout << "*p1 = " << *p1; // p1 = S
+	cout << "*p1 = " << *p1; // *p1 = 9772
 
 	cout << ", p1 = " << p1 << endl; // , p1 = S
 
@@ -70,11 +70,13 @@ int main()
 //	char c = 'A';
 //	double *p = &c;
 
-//   pointer must be the same variable type as what is being referenced
+//   pointer must be the same data type as what is being pointed at
 
 
 
-//  5. 
+//  5. Look at the following code. 
+//	Give the value of the left-hand side variable in each assignment statement.  
+//	Assume the lines are executed sequentially. Assume the address of the blocks array is 4434. 
 	
 
 		char blocks[3] = { 'A','B','C' };
@@ -101,6 +103,22 @@ int main()
 
 		return 0;
 
-	
+		//6. Write a function for each of the following descriptions.For each function, 
+		//use the pointer notation ONLY.Do NOT use the array index[] notation.
+
+			//A.Write a function RevString(char* array) which reverses array.The function returns nothing.
+		void RevString(char* array)
+		{
+
+		}
+
+			//B.Write a function CountEven(int* array, int array_len) which receives an integer array and its size, 
+			//and returns the number of even numbers in the array.
+
+			//C.Write a function Maximum(double* array, int array_size) that returns a pointer to the maximum value of an array of doubles.
+			//If the array is empty, return nullptr.
+
+			//D.Write a function Contains(char* array, char search_value) which returns true if the 1st parameter contains the 2nd parameter,
+			//or false otherwise.
 
 }
