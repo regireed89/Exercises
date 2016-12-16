bfe5f3f9ca965a925d99202df840a87a513088c4
regireed89 <regireed89@gmail.com>
2016-12-16 17:41:41 -0600

    loop

--- loops exercise/loops exercise/Source.cpp
+++ loops exercise/loops exercise/Source.cpp
@@ -1,72 +1,117 @@
-#include <iostream>
+#include <iostream> 
 using namespace std;
 
 int main()
 {
-
-	/*int x;
-	x = 100;
-
-	while (x == 100)
-		
-		x -= 2;
-		cout << x;*/
-
-
-
-		/*int y;
-
-		for (y = 0; y <= 100; y++)
+	/*{
+		int x;
+		x = 100;
+		while (x >= 0)
 		{
-			if (y % 3 == 0)
-			{
-				cout << "Fizz";
-			}
-			else if (y % 5 == 0)
-			{
-				cout << "Buzz";
-			}
-			else if (y % 3 == 0 && y % 5 == 0)
-			{
-				cout << "FizzBuzz";
-			}
-			else
-			{
-				cout << y;
-			}
 
-			std::cout << std::endl;
+			cout << x;
+			x -= 2;
 		}
-*/
+	}*/
 
-	int z;
-	int sum=0;
- 
-	for (z = 0; z < 1000; z++)
-	{
-		if (z % 3 == 0)
+
+
+	/*{
+		int x;
+		x = 100;
+
+		while (x >  0)
 		{
-			cout << z << " is a multiple of 3" << endl;
-			sum += z;
 
+			x -= 2;
+			cout << x;
 		}
+	}*/
 
-		if (z % 5 == 0)
+
+
+	/*{
+		int x;
+		x = 100;
+
+		while (x > 0)
 		{
-			cout << z << " is a multiple of 5" << endl;
-			sum += z;
-		}
-		cout << "sum @ loop:" << z << " is " << sum << endl;;
-		
 			
-		//266333
-			
+			cout << x;
+			x -= 2;
+		}
 		
+	}*/
 
-		
-	}
-		system("pause");
 	
+		/*{
+			int x;
+			x = 100;
+	
+			while (x >  0)
+			{
+	
+				x -= 2;
+				cout << x;
+			}
+		
+		}*/
+
+		//{
+		//	int y = 0;
+	 //
+		//	while (y <= 100)
+		//	{
+		//		y += 1;
+		//		if (y % 3 == 0)
+		//		{
+		//			cout << "Fizz";
+		//		}
+		//		else if (y % 5 == 0)
+		//		{
+		//			cout << "Buzz";
+		//		}
+		//		else if (y % 3 == 0 && y % 5 == 0)
+		//		{
+		//			cout << "FizzBuzz";
+		//		}
+		//		else
+		//		{
+		//			cout << y;
+		//		}
+	 //
+		//		std::cout << std::endl;
+		//	}
+
+		//}
+
+
+	//	int z;
+	//	int sum = 0;
+	//	z = 0;
+	//	while (z < 1000)
+	//	{
+	//		z += 1;
+	//		if (z % 3 == 0)
+	//		{
+	//			cout << z << " is a multiple of 3" << endl;
+	//			sum += z;
+	// 
+	//		}
+	// 
+	//	 	if (z % 5 == 0)
+	//		{
+	//			cout << z << " is a multiple of 5" << endl;
+	//			sum += z;
+	//		}
+	//		cout << "sum @ loop:" << z << " is " << sum << endl;;
+	//
+	//
+	//		//266333
+	//}
+
+	system("pause");
+
 }
 
 
