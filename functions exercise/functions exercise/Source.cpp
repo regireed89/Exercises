a512d46b4cef7329e1b46318bb4af8990d4b8d92
regireed89 <regireed89@gmail.com>
2016-12-28 22:20:08 -0600

    func

--- functions exercise/functions exercise/Source.cpp
+++ functions exercise/functions exercise/Source.cpp
@@ -2,83 +2,55 @@
 #include <cstdlib>
 
 //void PrintInteger(int variable)
-//
 //{
-//
 //	std::cout << variable << std::endl;
-//
 //}
-//
 //int main()
-//
 //{
-//
 //	int the_variable = 1;
-//
 //	PrintInteger(the_variable);
-//
 //	{
-//
 //		PrintInteger(the_variable);
-//
 //		int the_variable = 2;
-//
 //		PrintInteger(the_variable);
-//
 //		{
-//
 //			PrintInteger(the_variable);
-//
 //			int the_variable = 3;
-//
 //			PrintInteger(the_variable);
-//
 //		}
-//
 //		PrintInteger(the_variable);
-//
 //	}
-//
 //	PrintInteger(the_variable);
-//
-// /*
 //	    1
 //		1
 //		2
 //		2
 //		3
 //		2
-//		1*/
-// 
-//
+//		1
 //}
 
 //#2
-float smlValue(float a, float b,)
+float smlValue(float a, float b)
 {
-
-	std::cin >> a >> b;
-
 	if (a < b)
 	{
 		return a;
 	}
 	else
-
 	{
 		return b;
 	}
-
-
 	system("pause");
 }
+
 //#3
 // Funciton Overloads
 
 //#4
 float Half(float number)
 {
-	number = 16.721f;
+	
 
 	float result = number / 2;
 
@@ -86,7 +58,7 @@ float Half(float number)
 }
 
 //#5
-int CoinFlip()
+void CoinFlip()
 {
 	std::cout << "WELCOME TO COIN FLIP" << std::endl;
 	std::cout << "please choose 1 for heads or 2 for tails" << std::endl;
@@ -107,31 +79,67 @@ int CoinFlip()
 
 	if (f == 1)
 	{
-		std::cout << "COIN WAS TAILS" << std::endl;
+		std::cout << "COIN WAS HEADS" << std::endl;
 	}
 	if (f == 2)
 	{
-		std::cout << "COIN WAS HEADS" << std::endl;
+		std::cout << "COIN WAS TAILS" << std::endl;
 	}
-	char w = (x == f) ? printf("YOU WIN") : printf("YOU LOSE");
+	char w = (x == f) ? printf("YOU WIN") : printf("YOU LOSE ");
 	std::cout << w;
 
 system("pause");
 system("cls");
 }
 //#6
-int sum(int x, int y)
+//int sum(int x, int y)
+//{
+//	int result;
+//	result = x + y;
+//	//no return value
+//}
+
+//int sum(int n)
+//{
+//	if (0 == n)
+//		return 0;
+//	else
+//		n = n + n;//adding n to its self then assigning the sum to its self	
+//}
+
+//int main()
+//{
+//	double x = 13.6;
+//	std::cout << "square of 13.6 = " << square(x) << std::endl; //calling function before function definition
+//	//passing in a double when function takes in a float
+//}
+//int square(int x) 
+//{
+//	return x * x; 
+//}
+
+//7
+int SumTo(int N)
 {
-	int result;
-	result = x + y;
+	int y = 0;
+	for (int i = 1; i <= N; i++)
+	{
+		y += i;
+	}
+		std::cout << y;
+	
+	return y;
+	system("pause");
 }
+
 
 int main()
 {
-	float a;
-	float b;
-	smlValue(a, b);
+	
+	//smlValue(10, 15);
 
-	Half(number);
-	CoinFlip();
+	//Half(16.721f);
+	//CoinFlip();
+	SumTo(15);
+	
 }
