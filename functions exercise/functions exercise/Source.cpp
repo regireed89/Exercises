cb8bf6feab2483b490d1122d5452f5eeccf93b8f
regireed89 <regireed89@gmail.com>
2016-12-30 20:02:04 -0600

    func

--- functions exercise/functions exercise/Source.cpp
+++ functions exercise/functions exercise/Source.cpp
@@ -276,6 +276,119 @@ int Power(int a, int b)
 	return y;
 }
 
+//16
+int num16(int input[], int size)
+{
+	int i;
+	int prev;
+	int count = 0;
+	for (i = 0; i < size; i++)
+	{
+		input[i];
+		i--;
+		prev = input[i];
+		i++;
+		for (int j = input[i]; j <= input[i]; j++)
+		{	
+			if (prev == j)
+			{
+				count++;
+				
+			}
+			else if (prev < j || prev>j)
+			{
+				cout << count << prev << " ";
+				count = 0;
+				count++;
+			}
+		}
+	}
+	return count;
+}
+
+void RPS()
+{
+	std::cout << "WELCOME TO ROCK, PAPER, SCISSOR" << std::endl;
+	std::cout << "CHOOSE 1 FOR ROCK" << std::endl;
+	std::cout << "CHOOSE 2 FOR PAPER" << std::endl;
+	std::cout << "CHOOSE 3 FOR SCISSOR" << std::endl;
+	std::cout << "ROCK BEATS SCISSORS" << std::endl;
+	std::cout << "PAPER BEATS ROCK" << std::endl;
+	std::cout << "SCISSOR BEATS PAPER" << std::endl;
+
+	int x;
+	std::cin >> x;
+
+	if (x == 1)
+	{
+		std::cout << "you choose rock" << std::endl;
+		int y = rand() % 3 + 1;
+		std::cout << y;
+		if (y == 1)
+		{
+			std::cout << "computer choose rock" << std::endl;
+			std::cout << "ITS A TIE" << std::endl;
+		}
+		else if (y == 2)
+		{
+			std::cout << "computer choose paper" << std::endl;
+			std::cout << "YOU LOSE COMPUTER WINS" << std::endl;
+		}
+		else if (y == 3)
+		{
+			std::cout << "computer choose scissor" << std::endl;
+			std::cout << "YOU WIN COMPUTER LOSE" << std::endl;
+		}
+	}
+	if (x == 2)
+	{
+		std::cout << "you choose paper" << std::endl;
+		int y = rand() % 3 + 1;
+		std::cout << y;
+		if (y == 1)
+		{
+			std::cout << "computer choose rock" << std::endl;
+			std::cout << "YOU WIN COMPUTER LOSE" << std::endl;
+		}
+		else if (y == 2)
+		{
+			std::cout << "computer choose paper" << std::endl;
+			std::cout << "ITS A TIE" << std::endl;
+		}
+		else if (y == 3)
+		{
+			std::cout << "computer choose scissor" << std::endl;
+			std::cout << "COMPUTER WIN YOU LOSE" << std::endl;
+		}
+	}
+	if (x == 3)
+	{
+		std::cout << "you choose scissor" << std::endl;
+		int y = rand() % 3 + 1;
+		std::cout << y;
+		if (y == 1)
+		{
+			std::cout << "computer choose rock" << std::endl;
+			std::cout << "COMPUTER WINS YOU LOSE" << std::endl;
+		}
+		else if (y == 2)
+		{
+			std::cout << "computer choose paper" << std::endl;
+			std::cout << "YOU WIN COMPUTER LOSE" << std::endl;
+		}
+		else if (y == 3)
+		{
+			std::cout << "computer choose scissor" << std::endl;
+			std::cout << "ITS A TIE" << std::endl;
+		}
+	}
+	else
+	{
+		std::cout << "YOU TRASH TRY AGAIN" << std::endl;
+	}
+
+}
+
 int main()
 {
 
@@ -304,5 +417,8 @@ int main()
 	//int tom3[255] = {};
 	//Split(tom, 10, tom2, tom3);
 	//Power(3, 3);
+	//int ops[255] = {1,1,2,1,2,2,3,3,4, };
+	//num16(ops, 255);
+	RPS();
 	system("pause");
 }
